2c0a7574f59ad51ae431f52c88fc8d08fcd4aa57
Mateusz_Górny <59448497+Mefju08@users.noreply.github.com>
2025-05-25 14:13:41 +0200

    Initial commit

---
+++ Calculator.UnitTests/SumServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using Calculator.Services;
+using FluentAssertions;
+
+namespace Calculator.UnitTests
+{
+    public class SumServiceTests
+    {
+        [Theory]
+        [InlineData(1,3,4)]
+        public void SumService_WithTwoNumbers_ShouldReturnSum(int numA, int numB, int expectedResult)
+        {
+            var sumService = new SumService();
+            int result = sumService.Sum(numA, numB);
+
+            result.Should().Be(expectedResult);
+        }
+    }
+}
